275621a582807064e26309a1164f396028fa5ba2
Tianze Shan <shant2@bu.edu>
2022-02-02 16:31:21 -0500

    java hw1

---
+++ Java/src/Employee.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Employee {
+
+
+    String name;
+    int age;
+
+    public Employee(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
